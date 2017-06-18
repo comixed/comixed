ee1e655ff17ea0966c09d941a9123b0e2c3add65
Darryl L. Pierce <mcpierce@gmail.com>
2017-06-18 10:14:23 -0400

    Added the AddComicWorkerTask and unit test.

---
+++ src/main/java/org/comixed/tasks/AddComicWorkerTask.java
@@ -0,0 +1,80 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.tasks;
+
+import java.io.File;
+
+import org.comixed.library.model.Comic;
+import org.comixed.library.model.ComicFileHandler;
+import org.comixed.library.model.ComicFileHandlerException;
+import org.comixed.repositories.ComicRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@ConfigurationProperties(prefix = "comic-file.handlers")
+public class AddComicWorkerTask extends AbstractWorkerTask
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ComicFileHandler comicFileHandler;
+    @Autowired
+    private ComicRepository comicRepository;
+
+    File file;
+
+    @Override
+    public void startTask() throws WorkerTaskException
+    {
+        logger.debug("Adding file to library: " + file);
+
+        Comic result = new Comic();
+
+        try
+        {
+            result.setFilename(this.file.getAbsolutePath());
+            comicFileHandler.loadComic(result);
+            comicRepository.save(result);
+        }
+        catch (ComicFileHandlerException error)
+        {
+            throw new WorkerTaskException("Failed to load comic", error);
+        }
+    }
+
+    /**
+     * Sets the name of the file to be added.
+     * 
+     * @param file
+     *            the file
+     */
+    public void setFile(File file)
+    {
+        logger.debug("Setting filename: " + file.getName());
+        this.file = file;
+    }
+}
---
+++ src/test/java/org/comixed/tasks/AddComicWorkerTaskTest.java
@@ -0,0 +1,88 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.tasks;
+
+import java.io.File;
+
+import org.comixed.library.model.Comic;
+import org.comixed.library.model.ComicFileHandler;
+import org.comixed.library.model.ComicFileHandlerException;
+import org.comixed.repositories.ComicRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class AddComicWorkerTaskTest
+{
+    private static final String TEST_CBZ_FILE = "src/test/resources/example.cbz";
+
+    @InjectMocks
+    private AddComicWorkerTask task;
+
+    @Mock
+    private ComicFileHandler comicFileHandler;
+
+    @Mock
+    private ComicRepository comicRepository;
+
+    @Captor
+    private ArgumentCaptor<Comic> comic;
+
+    @Test
+    public void testAddFile() throws WorkerTaskException, ComicFileHandlerException
+    {
+        Mockito.doNothing().when(comicFileHandler).loadComic(Mockito.any(Comic.class));
+        Mockito.when(comicRepository.save(Mockito.any(Comic.class))).thenReturn(Mockito.any(Comic.class));
+
+        File file = new File(TEST_CBZ_FILE);
+
+        task.file = file;
+        task.startTask();
+
+        Mockito.verify(comicFileHandler, Mockito.times(1)).loadComic(comic.capture());
+        Mockito.verify(comicRepository, Mockito.times(1)).save(comic.capture());
+    }
+
+    @Test(expected = WorkerTaskException.class)
+    public void testAddFileComicFileHandlerException() throws ComicFileHandlerException, WorkerTaskException
+    {
+        Mockito.doThrow(ComicFileHandlerException.class).when(comicFileHandler).loadComic(Mockito.any(Comic.class));
+
+        try
+        {
+            File file = new File(TEST_CBZ_FILE);
+
+            task.file = file;
+            task.startTask();
+        }
+        finally
+        {
+            Mockito.verify(comicFileHandler, Mockito.times(1)).loadComic(comic.capture());
+        }
+    }
+}
