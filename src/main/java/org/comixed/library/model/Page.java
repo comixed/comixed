d262d915840c368804e091d729a6bcb14792b649
Darryl L. Pierce <mcpierce@gmail.com>
2017-06-18 14:28:50 -0400

    Added the Page class and unit tests.

---
+++ src/main/java/org/comixed/library/model/Page.java
@@ -0,0 +1,204 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.model;
+
+import java.awt.Image;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.persistence.Transient;
+import javax.swing.ImageIcon;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <code>Page</code> represents a single page from a comic.
+ *
+ * @author Darryl L. Pierce
+ *
+ */
+public class Page
+{
+    @Transient
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Comic comic;
+    private String filename;
+    private byte[] content;
+    private String hash;
+    private ImageIcon icon;
+
+    /**
+     * Default constructor.
+     */
+    public Page()
+    {}
+
+    /**
+     * Creates a new instance with the given filename and image content.
+     *
+     * @param filename
+     *            the filename
+     * @param content
+     *            the content
+     */
+    public Page(String filename, byte[] content)
+    {
+        this.logger.debug("Creating page: filename=" + filename + " content.size=" + content.length);
+        this.filename = filename;
+        this.content = content;
+        this.hash = this.createHash(content);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (this.getClass() != obj.getClass()) return false;
+        Page other = (Page )obj;
+        if (this.filename == null)
+        {
+            if (other.filename != null) return false;
+        }
+        else if (!this.filename.equals(other.filename)) return false;
+        if (this.hash == null)
+        {
+            if (other.hash != null) return false;
+        }
+        else if (!this.hash.equals(other.hash)) return false;
+        return true;
+    }
+
+    /**
+     * Returns the owning comic.
+     *
+     * @return the comic
+     */
+    public Comic getComic()
+    {
+        return this.comic;
+    }
+
+    /**
+     * Returns the content for the page.
+     *
+     * @return the content
+     */
+    public byte[] getContent()
+    {
+        return this.content;
+    }
+
+    /**
+     * Returns the filename for the page.
+     *
+     * @return the filename
+     */
+    public String getFilename()
+    {
+        return this.filename;
+    }
+
+    public String getHash()
+    {
+        return this.hash;
+    }
+
+    /**
+     * Returns the original image for the page.
+     *
+     * @return the image
+     */
+    public ImageIcon getImage()
+    {
+        if (this.icon == null)
+        {
+            this.logger.debug("Generating image from content");
+            this.icon = new ImageIcon(this.content);
+        }
+        return this.icon;
+    }
+
+    /**
+     * Returns the image, resized so that the width is the specified width.
+     *
+     * @param width
+     *            the maximum width
+     * @return the resized image
+     */
+    public ImageIcon getImage(int width)
+    {
+        ImageIcon image = this.getImage();
+        int oldWidth = image.getIconWidth();
+        int oldHeight = image.getIconHeight();
+        int height = (int )(((float )width / (float )oldWidth) * oldHeight);
+        this.logger.debug("Fetching resized image: " + width + "x" + height);
+
+        return new ImageIcon(this.icon.getImage().getScaledInstance(width, height, Image.SCALE_FAST));
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((this.filename == null) ? 0 : this.filename.hashCode());
+        result = (prime * result) + ((this.hash == null) ? 0 : this.hash.hashCode());
+        return result;
+    }
+
+    /**
+     * Sets a new filename for the page.
+     *
+     * @param filename
+     *            the new filename
+     */
+    public void setFilename(String filename)
+    {
+        this.logger.debug("Changing filename: " + this.filename + " -> " + filename);
+        this.filename = filename;
+    }
+
+    private String createHash(byte[] bytes)
+    {
+        this.logger.debug("Generating MD5 hash");
+        String result = "";
+        MessageDigest md;
+        try
+        {
+            md = MessageDigest.getInstance("MD5");
+            md.update(bytes);
+            result = new BigInteger(1, md.digest()).toString(16).toUpperCase();
+        }
+        catch (NoSuchAlgorithmException error)
+        {
+            this.logger.error("Failed to generate hash", error);
+        }
+        this.logger.debug("Returning: " + result);
+        return result;
+    }
+
+    void setComic(Comic comic)
+    {
+        this.comic = comic;
+    }
+}
---
+++ src/test/java/org/comixed/library/model/PageTest.java
@@ -0,0 +1,121 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.model;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.swing.ImageIcon;
+
+import org.h2.util.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PageTest
+{
+    private static final String TEST_JPG_FILE = "src/test/resources/example.jpg";
+    private Page page;
+    private static String expected_hash;
+    private static byte[] content;
+
+    static
+    {
+        File file = new File(TEST_JPG_FILE);
+        content = new byte[(int )file.length()];
+        FileInputStream input;
+        try
+        {
+            input = new FileInputStream(file);
+            IOUtils.readFully(input, content, content.length);
+            input.close();
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(content);
+            expected_hash = new BigInteger(1, md.digest()).toString(16).toUpperCase();
+        }
+        catch (IOException
+               | NoSuchAlgorithmException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Before
+    public void setUp() throws IOException
+    {
+        page = new Page(TEST_JPG_FILE, content);
+    }
+
+    @Test
+    public void testHasFilename()
+    {
+        assertEquals(TEST_JPG_FILE, page.getFilename());
+    }
+
+    @Test
+    public void testCanUpdateFilename()
+    {
+        String filename = TEST_JPG_FILE.substring(1);
+        page.setFilename(filename);
+
+        assertEquals(filename, page.getFilename());
+    }
+
+    @Test
+    public void testHasContent()
+    {
+        assertNotNull(page.getContent());
+        assertArrayEquals(content, page.getContent());
+    }
+
+    @Test
+    public void testHasHash()
+    {
+        assertEquals(expected_hash, page.getHash());
+    }
+
+    @Test
+    public void testHasImage()
+    {
+        ImageIcon result = page.getImage();
+
+        assertNotNull(result);
+        assertEquals(338, result.getIconWidth());
+        assertEquals(479, result.getIconHeight());
+    }
+
+    @Test
+    public void testCanResizeImages()
+    {
+        ImageIcon result = page.getImage(169);
+
+        assertNotNull(result);
+        assertEquals(169, result.getIconWidth());
+        assertEquals(239, result.getIconHeight());
+    }
+}
