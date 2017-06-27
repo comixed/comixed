bf21eb97c5333477335ff917f9fad2a906ec7547
Darryl L. Pierce <mcpierce@gmail.com>
2017-06-27 09:08:12 -0400

    Added the ComicTableModel class and unit tests.

---
+++ src/main/java/org/comixed/library/model/ComicTableModel.java
@@ -0,0 +1,143 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.model;
+
+import java.util.Locale;
+
+import javax.swing.table.DefaultTableModel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+/**
+ * <code>ComicTableModel</code> provides the model for viewing the set of comics
+ * in the library.
+ * 
+ * @author Darryl L. Pierce
+ *
+ */
+@Component
+public class ComicTableModel extends DefaultTableModel
+{
+    private static final int TEAMS = 13;
+    private static final int SUMMARY = 12;
+    private static final int STORY_ARCS = 11;
+    private static final int PAGE_COUNT = 10;
+    private static final int LOCATIONS = 9;
+    private static final int ISSUE_NUMBER = 8;
+    private static final int FILENAME = 7;
+    private static final int DESCRIPTION = 6;
+    private static final int LAST_READ_DATE = 5;
+    private static final int ADDED_DATE = 4;
+    private static final int COVER_DATE = 3;
+    private static final int PUBLISHER = 2;
+    private static final int VOLUME = 1;
+    private static final int NAME = 0;
+    private static final long serialVersionUID = -2124724909306232112L;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    static final String[] COLUMN_NAMES =
+    {"name",
+     "volume",
+     "publisher",
+     "date_published",
+     "date_added",
+     "date_last_ready",
+     "description",
+     "filename",
+     "issue_number",
+     "locations",
+     "pages",
+     "story_arcs",
+     "summary",
+     "teams"};
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private ComicSelectionModel comicSelectionModel;
+
+    @Override
+    public int getRowCount()
+    {
+        return comicSelectionModel != null ? comicSelectionModel.getComicCount() : 0;
+    }
+
+    @Override
+    public int getColumnCount()
+    {
+        return COLUMN_NAMES.length;
+    }
+
+    @Override
+    public String getColumnName(int column)
+    {
+        return messageSource.getMessage("view.table." + COLUMN_NAMES[column] + ".label", null, Locale.getDefault());
+    }
+
+    @Override
+    public Object getValueAt(int row, int column)
+    {
+        logger.debug("Getting value at " + row + "x" + column);
+        Comic comic = (comicSelectionModel != null) ? comicSelectionModel.getComic(row) : null;
+
+        if (comic != null)
+        {
+            switch (column)
+            {
+                case NAME:
+                    return comic.getName();
+                case VOLUME:
+                    return comic.getVolume();
+                case PUBLISHER:
+                    return comic.getPublisher();
+                case COVER_DATE:
+                    return comic.getCoverDate();
+                case ADDED_DATE:
+                    return comic.getDateAdded();
+                case LAST_READ_DATE:
+                    return comic.getDateLastRead();
+                case DESCRIPTION:
+                    return comic.getDescription();
+                case FILENAME:
+                    return comic.getFilename();
+                case ISSUE_NUMBER:
+                    return comic.getIssueNumber();
+                case LOCATIONS:
+                    return comic.getLocations();
+                case PAGE_COUNT:
+                    return comic.getPageCount();
+                case STORY_ARCS:
+                    return comic.getStoryArcs();
+                case SUMMARY:
+                    return comic.getSummary();
+                case TEAMS:
+                    return comic.getTeams();
+            }
+        }
+
+        return null;
+    }
+
+}
---
+++ src/test/java/org/comixed/library/model/ComicTableModelTest.java
@@ -0,0 +1,100 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class ComicTableModelTest
+{
+    private static final int TEST_COMIC_COUNT = 17;
+    private static final String TEST_COMIC_NAME = "Comic Name";
+    private static final int TEST_COMIC_ROW = 7;
+
+    @InjectMocks
+    private ComicTableModel comicTableModel;
+
+    @Mock
+    private ComicSelectionModel comicSelectionModel;
+
+    @Mock
+    private MessageSource messageSource;
+
+    @Mock
+    private Comic comic;
+
+    @Test
+    public void testGetRowCount()
+    {
+        Mockito.when(comicSelectionModel.getComicCount()).thenReturn(TEST_COMIC_COUNT);
+
+        assertEquals(TEST_COMIC_COUNT, comicTableModel.getRowCount());
+
+        Mockito.verify(comicSelectionModel, Mockito.times(1)).getComicCount();
+    }
+
+    @Test
+    public void testGetColumnCount()
+    {
+        assertEquals(ComicTableModel.COLUMN_NAMES.length, comicTableModel.getColumnCount());
+    }
+
+    @Test
+    public void testGetColumnName()
+    {
+        for (int index = 0;
+             index < ComicTableModel.COLUMN_NAMES.length;
+             index++)
+        {
+            String name = ComicTableModel.COLUMN_NAMES[index];
+            String label = "view.table." + name + ".label";
+            Mockito.when(messageSource.getMessage(Mockito.anyString(), Mockito.any(), Mockito.any(Locale.class)))
+                   .thenReturn(name);
+
+            assertEquals(name, comicTableModel.getColumnName(index));
+
+            Mockito.verify(messageSource, Mockito.times(1)).getMessage(label, null, Locale.getDefault());
+        }
+    }
+
+    @Test
+    public void testGetValueAt()
+    {
+        Mockito.when(comicSelectionModel.getComic(Mockito.anyInt())).thenReturn(comic);
+        Mockito.when(comic.getName()).thenReturn(TEST_COMIC_NAME);
+
+        assertEquals(TEST_COMIC_NAME, comicTableModel.getValueAt(TEST_COMIC_ROW, 0));
+
+        Mockito.verify(comicSelectionModel, Mockito.times(1)).getComic(TEST_COMIC_ROW);
+        Mockito.verify(comic, Mockito.times(1)).getName();
+    }
+}
