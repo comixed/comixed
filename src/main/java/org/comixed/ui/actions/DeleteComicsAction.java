ddc17502f58da41241f234b1746ceacc6c7143bc
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-20 10:24:08 -0400

    fixup! Added the File->Delete menu item and supporting classes.

--- src/main/java/org/comixed/ui/actions/DeleteComicsAction.java
+++ src/main/java/org/comixed/ui/actions/DeleteComicsAction.java
@@ -1,17 +1,17 @@
 /*
  * ComixEd - A digital comic book library management application.
  * Copyright (C) 2017, Darryl L. Pierce
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.package
  * org.comixed;
@@ -28,6 +28,7 @@ import javax.swing.AbstractAction;
 import javax.swing.JOptionPane;
 
 import org.comixed.library.model.Comic;
+import org.comixed.library.model.ComicSelectionListener;
 import org.comixed.library.model.ComicSelectionModel;
 import org.comixed.tasks.DeleteComicsWorkerTask;
 import org.comixed.tasks.Worker;
@@ -35,6 +36,7 @@ import org.comixed.ui.ComicTableView;
 import org.comixed.ui.MainFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -43,12 +45,14 @@ import org.springframework.stereotype.Component;
 /**
  * <code>DeleteComicsAction</code> prompts the user and, if the user agrees,
  * deletes the comics they have selected.
- * 
+ *
  * @author Darryl L. Pierce
  *
  */
 @Component
-public class DeleteComicsAction extends AbstractAction
+public class DeleteComicsAction extends AbstractAction implements
+                                InitializingBean,
+                                ComicSelectionListener
 {
     private static final long serialVersionUID = -2294529876125471390L;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -74,44 +78,66 @@ public class DeleteComicsAction extends AbstractAction
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        int[] rows = comicTableView.getSelectedRows();
+        int[] rows = this.comicTableView.getSelectedRows();
         List<Comic> comics = new ArrayList<>();
 
         for (int row : rows)
         {
-            comics.add(comicSelectionModel.getComic(row));
+            comics.add(this.comicSelectionModel.getComic(row));
         }
 
-        logger.debug("Prompting the user to delete " + comics.size() + " comics");
+        this.logger.debug("Prompting the user to delete " + comics.size() + " comics");
 
-        if (JOptionPane.showConfirmDialog(mainFrame,
-                                          messageSource.getMessage("dialog.confirm.delete.message", new Object[]
+        if (JOptionPane.showConfirmDialog(this.mainFrame,
+                                          this.messageSource.getMessage("dialog.confirm.delete.message", new Object[]
                                           {comics.size()}, Locale.getDefault()),
-                                          messageSource.getMessage("dialog.confirm.delete.title", null,
-                                                                   Locale.getDefault()),
+                                          this.messageSource.getMessage("dialog.confirm.delete.title", null,
+                                                                        Locale.getDefault()),
                                           JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
         {
-            boolean deleteFileAsWell = (JOptionPane.showConfirmDialog(mainFrame,
-                                                                      messageSource.getMessage("dialog.confirm.delete.include-file.label",
-                                                                                               new Object[]
-                                                                                               {comics.size()},
-                                                                                               Locale.getDefault()),
-                                                                      messageSource.getMessage("dialog.confirm.delete.include-file.title",
-                                                                                               null,
-                                                                                               Locale.getDefault()),
+            boolean deleteFileAsWell = (JOptionPane.showConfirmDialog(this.mainFrame,
+                                                                      this.messageSource.getMessage("dialog.confirm.delete.include-file.label",
+                                                                                                    new Object[]
+                                                                                                    {comics.size()},
+                                                                                                    Locale.getDefault()),
+                                                                      this.messageSource.getMessage("dialog.confirm.delete.include-file.title",
+                                                                                                    null,
+                                                                                                    Locale.getDefault()),
                                                                       JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION);
             {
-                logger.debug("Deleting comics...");
-                DeleteComicsWorkerTask task = taskFactory.getObject();
+                this.logger.debug("Deleting comics...");
+                DeleteComicsWorkerTask task = this.taskFactory.getObject();
 
                 task.setComics(comics);
                 task.setDeleteFiles(deleteFileAsWell);
-                worker.addTasksToQueue(task);
+                this.worker.addTasksToQueue(task);
             }
         }
         else
         {
-            logger.debug("User declined to delete comics...");
+            this.logger.debug("User declined to delete comics...");
         }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        this.comicSelectionModel.addComicSelectionListener(this);
+        this.toggleEnabled();
+    }
+
+    @Override
+    public void comicListChanged()
+    {/* ignored */ }
+
+    @Override
+    public void selectionChanged()
+    {
+        this.toggleEnabled();
+    }
+
+    private void toggleEnabled()
+    {
+        this.setEnabled(this.comicSelectionModel.hasSelections());
     }
 }
