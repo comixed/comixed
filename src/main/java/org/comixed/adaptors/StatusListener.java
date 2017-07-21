7d58ee23f3ed57d04ac2ed46ebb2b69466f8a94f
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-21 15:27:28 -0400

    Added the StatusAdaptor class and listener.

--- src/main/java/org/comixed/adaptors/StatusListener.java
+++ src/main/java/org/comixed/adaptors/StatusListener.java
@@ -1,34 +1,38 @@
 /*
  * ComixEd - A digital comic book library management application.
  * Copyright (C) 2017, Darryl L. Pierce
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.package
  * org.comixed;
  */
 
-package org.comixed;
+package org.comixed.adaptors;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages =
-{"org.comixed.adaptors",
- "org.comixed.library",
- "org.comixed.repositories",
- "org.comixed.tasks"})
-public class ComixEdTestContext
+/**
+ * <code>StatusListener</code> defines a type that receives notification of
+ * status message updates.
+ *
+ * @author Darryl L. Pierce
+ *
+ */
+public interface StatusListener
 {
-
+    /**
+     * Invoked when a new status message is to be displayed.
+     *
+     * @param message
+     *            the status message
+     */
+    void statusTextChanged(String message);
 }
