39b9c9fd1b3d4c25ab9a9774fc7d6958fc0de12c
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-21 08:24:01 -0400

    Moved the main menu defintion to a separate file.

--- src/main/java/org/comixed/ui/MainMenuBar.java
+++ src/main/java/org/comixed/ui/MainMenuBar.java
@@ -32,6 +32,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -42,6 +43,7 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@PropertySource("classpath:menus.properties")
 @ConfigurationProperties("app.menus")
 public class MainMenuBar extends JMenuBar implements
                          InitializingBean
