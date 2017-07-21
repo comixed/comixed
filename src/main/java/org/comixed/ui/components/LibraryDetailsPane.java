32bd10bfe2b02d795f420aede0e5b9fdc40db858
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-21 11:05:16 -0400

    Added the LibraryDetailsPane class.

---
+++ src/main/java/org/comixed/ui/components/LibraryDetailsPane.java
@@ -0,0 +1,112 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.ui.components;
+
+import java.util.Locale;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+import org.comixed.library.model.ComicSelectionListener;
+import org.comixed.library.model.ComicSelectionModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+/**
+ * <code>LibraryDetailsPane</code> shows details about the current state of the
+ * library.
+ *
+ * @author Darryl L. Pierce
+ *
+ */
+@Component
+public class LibraryDetailsPane extends JPanel implements
+                                InitializingBean,
+                                ComicSelectionListener
+{
+    private static final long serialVersionUID = -8742844084547538845L;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private ComicSelectionModel comicSelectionModel;
+
+    private JLabel totalComics = new JLabel();
+    private JLabel displayedComics = new JLabel();
+
+    public LibraryDetailsPane()
+    {
+        super();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        this.comicSelectionModel.addComicSelectionListener(this);
+        this.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        this.buildLayout();
+        this.updateDisplayedComicCount();
+        this.updateTotalComicCount();
+    }
+
+    private void buildLayout()
+    {
+        this.logger.debug("Laying out UI components");
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.add(this.totalComics);
+        this.add(this.displayedComics);
+    }
+
+    @Override
+    public void comicListChanged()
+    {
+        this.updateTotalComicCount();
+    }
+
+    @Override
+    public void selectionChanged()
+    {
+        this.updateDisplayedComicCount();
+    }
+
+    private void updateDisplayedComicCount()
+    {
+        this.logger.debug("Updating displayed comic count");
+        this.displayedComics.setText(this.messageSource.getMessage("view.details.library.displayed-comics.text",
+                                                                   new Object[]
+                                                                   {this.comicSelectionModel.getComicCount()},
+                                                                   this.getLocale()));
+    }
+
+    private void updateTotalComicCount()
+    {
+        this.logger.debug("Updating total comic count");
+        this.totalComics.setText(this.messageSource.getMessage("view.details.library.total-comics.text", new Object[]
+        {this.comicSelectionModel.getTotalComics()}, this.getLocale()));
+    }
+}
