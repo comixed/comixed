7e3f3d323564031e52842f12f90ce131e22e591b
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-21 10:42:49 -0400

    Added the DetailsPane to the main window client area.
    The pane is separated by a split pane, whose position is stored in the
    configuration file.

--- src/main/java/org/comixed/ui/components/MainClientPanel.java
+++ src/main/java/org/comixed/ui/components/MainClientPanel.java
@@ -1,17 +1,17 @@
 /*
  * ComixEd - A digital comic book library management application.
  * Copyright (C) 2017, Darryl L. Pierce
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.package
  * org.comixed;
@@ -23,7 +23,9 @@ import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 
+import org.comixed.AppConfiguration;
 import org.comixed.ui.ComicTableView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +36,7 @@ import org.springframework.stereotype.Component;
 /**
  * <code>MainClientPanel</code> manages the main portion of the application's
  * window.
- * 
+ *
  * @author Darryl L. Pierce
  *
  */
@@ -43,10 +45,19 @@ public class MainClientPanel extends JPanel implements
                              InitializingBean
 {
     private static final long serialVersionUID = -6194561704845594236L;
+    private static final String MAIN_CLIENT_DIVIDER_LOCATION = "window.main.client.divider.location";
+
+    private static final String MAIN_CLIENT_LAST_DIVIDER_LOCATION = "window.main.client.divider.last.location";
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
+    private DetailsPane detailsPane;
+
+    @Autowired
     private ComicTableView tableView;
+
+    @Autowired
+    private AppConfiguration configuration;
 
     public MainClientPanel()
     {
@@ -56,7 +67,34 @@ public class MainClientPanel extends JPanel implements
     @Override
     public void afterPropertiesSet() throws Exception
     {
-        logger.debug("Setting up table view");
-        this.add(new JScrollPane(tableView), BorderLayout.CENTER);
+        this.logger.debug("Setting up table view");
+        this.createLayout();
+    }
+
+    private void createLayout()
+    {
+        JSplitPane splitpane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(this.detailsPane),
+                                              new JScrollPane(this.tableView));
+        this.add(splitpane, BorderLayout.CENTER);
+
+        // restore layout
+        if (this.configuration.hasOption(MAIN_CLIENT_DIVIDER_LOCATION))
+        {
+            splitpane.setDividerLocation(Integer.parseInt(this.configuration.getOption(MAIN_CLIENT_DIVIDER_LOCATION)));
+        }
+        if (this.configuration.hasOption(MAIN_CLIENT_LAST_DIVIDER_LOCATION))
+        {
+            splitpane.setLastDividerLocation(Integer.parseInt(this.configuration.getOption(MAIN_CLIENT_LAST_DIVIDER_LOCATION)));
+        }
+
+        // add listener
+        splitpane.addPropertyChangeListener(evt ->
+        {
+            MainClientPanel.this.configuration.setOption(MAIN_CLIENT_DIVIDER_LOCATION,
+                                                         String.valueOf(splitpane.getDividerLocation()));
+            MainClientPanel.this.configuration.setOption(MAIN_CLIENT_LAST_DIVIDER_LOCATION,
+                                                         String.valueOf(splitpane.getLastDividerLocation()));
+            MainClientPanel.this.configuration.save();
+        });
     }
 }
