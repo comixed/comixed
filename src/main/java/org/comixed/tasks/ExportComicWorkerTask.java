47a027c9d67cbb56fc9196c6dd4d70afae7e09b8
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-21 16:53:18 -0400

    ExportComicWorkerTask is now a proper Prototype bean.

--- src/main/java/org/comixed/tasks/ExportComicWorkerTask.java
+++ src/main/java/org/comixed/tasks/ExportComicWorkerTask.java
@@ -31,9 +31,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class ExportComicWorkerTask extends AbstractWorkerTask
 {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
