ca5c6f63ee3eebce59c64ace5fcc5e4a8f20394c
Darryl L. Pierce <mcpierce@gmail.com>
2017-07-26 09:26:29 -0400

    Renamed EditExportZipAction -> ExportToZipAction.

--- src/main/java/org/comixed/ui/actions/ExportToZipAction.java
+++ src/main/java/org/comixed/ui/actions/ExportToZipAction.java
@@ -37,7 +37,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EditExportZipAction extends AbstractAction implements
+public class ExportToZipAction extends AbstractAction implements
                                  InitializingBean,
                                  ComicSelectionListener
 {
