467a7e8f33deba916a25ddc0fadfc3994c924c08
Darryl L. Pierce <mcpierce@gmail.com>
2018-04-18 08:57:23 -0400

    [Issue #29] Added the ComicService to the list of providers.

--- web-frontend/src/app/app.component.ts
+++ web-frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import {ComicService} from './comic/comic.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ComicService],
 })
 export class AppComponent {
   title = 'app';
