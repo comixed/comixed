3233cef2e58913ecbde3a9280403dc247ce38f83
Darryl L. Pierce <mcpierce@gmail.com>
2018-05-03 11:19:56 -0400

    [Issue #62] Added a dependency on ng-bootstrap.

--- web-frontend/src/app/app.module.ts
+++ web-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -7,15 +8,18 @@ import {HttpModule} from '@angular/http';
 
 import {ComicModule} from './comic/comic.module';
 import {MainPageComponent} from './main-page/main-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AppRoutingModule,
     HttpModule,
     ComicModule,
