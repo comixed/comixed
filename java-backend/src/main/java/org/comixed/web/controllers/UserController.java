1ee273c458f37884a7498e3fc7d1a09a41d7fc35
Darryl L. Pierce <mcpierce@gmail.com>
2018-05-27 09:58:08 -0400

    [Issue #42] Added the UserController class.

---
+++ java-backend/src/main/java/org/comixed/web/controllers/UserController.java
@@ -0,0 +1,39 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.web.controllers;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController
+{
+    @RequestMapping(value = "/user",
+                    method = RequestMethod.GET)
+    @CrossOrigin
+    public Principal getCurrentUser(Principal user)
+    {
+        return user;
+    }
+}
