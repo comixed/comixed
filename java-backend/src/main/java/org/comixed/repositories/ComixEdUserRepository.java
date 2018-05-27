42c12e1ebe139971867f27493742bb373c2c6f5b
Darryl Pierce <mcpierce@gmail.com>
2018-05-26 22:44:33 -0400

    [Issue #42] Created the ComixEdUserRepository type.

---
+++ java-backend/src/main/java/org/comixed/repositories/ComixEdUserRepository.java
@@ -0,0 +1,30 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.repositories;
+
+import org.comixed.library.model.ComixEdUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComixEdUserRepository extends
+                                       CrudRepository<ComixEdUser,
+                                                      Long>
+{
+    ComixEdUser findByEmail(String email);
+}
