e9f92f47a466f3717babc0c15929294991e7b11f
Darryl Pierce <mcpierce@gmail.com>
2018-06-10 15:11:28 -0400

    [Issue #95] Added APIs for getting and settng user preferences.

--- java-backend/src/main/java/org/comixed/web/controllers/UserController.java
+++ java-backend/src/main/java/org/comixed/web/controllers/UserController.java
@@ -21,11 +21,16 @@ package org.comixed.web.controllers;
 
 import java.security.Principal;
 
+import org.comixed.library.model.ComixEdUser;
+import org.comixed.repositories.ComixEdUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,11 +38,39 @@ public class UserController
 {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private ComixEdUserRepository userRepository;
+
     @RequestMapping(value = "/api/user",
                     method = RequestMethod.GET)
     @CrossOrigin
     public Principal getCurrentUser(Principal user)
     {
         return user;
+    }
+
+    @RequestMapping(value = "/api/user/property",
+                    method = RequestMethod.GET)
+    public String getUserProperty(Authentication authentication, @RequestParam("name") String name)
+    {
+        this.logger.debug("Loading user: email={}", authentication.getName());
+        ComixEdUser user = this.userRepository.findByEmail(authentication.getName());
+
+        this.logger.debug("Return property: {}={}", name, user.getPreference(name));
+        return user.getPreference(name);
+    }
+
+    @RequestMapping(value = "/api/user/property",
+                    method = RequestMethod.POST)
+    public void setUserProperty(Authentication authentication,
+                                @RequestParam("name") String name,
+                                @RequestParam("value") String value)
+    {
+        this.logger.debug("Loading user: email={}", authentication.getName());
+        ComixEdUser user = this.userRepository.findByEmail(authentication.getName());
+
+        this.logger.debug("Setting property: {}={}", name, value);
+        user.setProperty(name, value);
+        this.userRepository.save(user);
     }
 }
