1cf16d3cad12c308311f42a4d3e4515b0dbc0ea7
Darryl L. Pierce <mcpierce@gmail.com>
2018-07-25 08:04:17 -0400

    [Issue #121] Added the FilenameScraperAdaptor class and tests.

---
+++ java-backend/src/main/java/org/comixed/library/adaptors/FilenameScraperAdaptor.java
@@ -0,0 +1,168 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2017, Darryl L. Pierce
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.adaptors;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FilenameUtils;
+import org.comixed.library.model.Comic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * <code>FilenameScraperAdaptor</code> scrapes comic meta-information from the
+ * filename for a comic.
+ *
+ * @author Darryl L. Pierce
+ *
+ */
+@Component
+public class FilenameScraperAdaptor
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static class RuleSet
+    {
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+        public final Pattern expression;
+        public final int series;
+        public final int volume;
+        public final int issue;
+        public int coverDate;
+        public SimpleDateFormat dateFormat;
+
+        public RuleSet(String expression, int series, int volume, int issue, int coverDate, String dateFormat)
+        {
+            this.expression = Pattern.compile(expression);
+            this.series = series;
+            this.volume = volume;
+            this.issue = issue;
+            this.coverDate = coverDate;
+            this.dateFormat = new SimpleDateFormat(dateFormat);
+            logger.debug("Creating ruleset: expression={} series={} volume={} issue={} coverDate={} dateFormat={}",
+                         expression, series, volume, issue, coverDate, dateFormat);
+        }
+
+        public boolean applies(String filename)
+        {
+            return this.expression.matcher(filename).matches();
+        }
+
+        public String[] process(String filename)
+        {
+            this.logger.debug("Processing filename: {}", filename);
+            Matcher matches = this.expression.matcher(filename);
+            String[] result = new String[matches.groupCount() + 1];
+
+            while (matches.find())
+            {
+                for (int index = 0;
+                     index < result.length;
+                     index++)
+                {
+                    result[index] = matches.group(index);
+                    this.logger.debug("Setting index={} to {}", index, result[index]);
+                }
+            }
+
+            return result;
+        }
+
+        public Date parseCoverDate(String dateString) throws ParseException
+        {
+            return this.dateFormat.parse(dateString);
+        }
+    }
+
+    // SERIES Vol.VVVV #nnn (Month, YYYY)
+    private static final RuleSet RULESET1 = new RuleSet("^(([\\w[\\s][,-]]+)?(\\sVol\\.))([0-9]{4}).*\\#([0-9]{1,5}).*\\(([a-zA-Z]+, [0-9]{4})\\).*$",
+                                                        2, 4, 5, 6, "MMMMM, yyyy");
+    // SERIES nnn (VVVV)
+    private static final RuleSet RULESET2 = new RuleSet("^([\\w[\\s][,-]]+)\\s([0-9]{1,5})\\s+\\(([0-9]{4})\\).*$", 1,
+                                                        3, 2, -1, "");
+    // SERIES (VVVV)
+    private static final RuleSet RULESET3 = new RuleSet("^([\\w[\\s][,-]]+)\\s+\\(([0-9]{4})\\).*$", 1, 2, -1, -1, "");
+    private static final RuleSet[] RULESET = new RuleSet[]
+    {RULESET1,
+     RULESET2,
+     RULESET3,};
+
+    private boolean applyRule(Comic comic, String filename, RuleSet ruleset)
+    {
+        boolean result = false;
+
+        if (ruleset.applies(filename))
+        {
+            String[] elements = ruleset.process(filename);
+            if (ruleset.series >= 0)
+            {
+                comic.setSeries(elements[ruleset.series]);
+            }
+            if (ruleset.volume >= 0)
+            {
+                comic.setVolume(elements[ruleset.volume]);
+            }
+            if (ruleset.issue >= 0)
+            {
+                comic.setIssueNumber(elements[ruleset.issue]);
+            }
+            if (ruleset.coverDate != -1)
+            {
+                try
+                {
+                    comic.setCoverDate(ruleset.parseCoverDate(elements[ruleset.coverDate]));
+                }
+                catch (ParseException error)
+                {
+                    this.logger.error("Failed to parse date string: " + elements[ruleset.coverDate], error);
+                }
+            }
+            result = true;
+        }
+
+        return result;
+    }
+
+    /**
+     * Sets the meta-information on the comic based on the comic's filename.
+     *
+     * @param comic
+     *            the comic to be updated
+     */
+    public void execute(Comic comic)
+    {
+        String filename = FilenameUtils.getName(comic.getFilename());
+        this.logger.debug("Attempting to extract comic meta-data from filename: {}", filename);
+
+        boolean done = false;
+        for (int index = 0;
+             !done && (index < RULESET.length);
+             index++)
+        {
+            this.logger.debug("Attempting to use ruleset #{}", index);
+            done = (this.applyRule(comic, filename, RULESET[index]));
+        }
+    }
+}
---
+++ java-backend/src/test/java/org/comixed/library/adaptors/FilenameScraperAdaptorTest.java
@@ -0,0 +1,106 @@
+/*
+ * ComixEd - A digital comic book library management application.
+ * Copyright (C) 2018, Darryl L. Pierce
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.library.adaptors;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.comixed.library.model.Comic;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class FilenameScraperAdaptorTest
+{
+    private static final String FILE_LOCATION = "/User/home/comixed_user/";
+    private static final String SERIES_NAME = "Batman - Sins of the Father";
+    private static final String VOLUME_NAME = "2018";
+    private static final String ISSUE_NUMBER = "012";
+    private static final String COVER_DATE_1 = "May, 2018";
+    private static final String COMIC_FILENAME_RULESET_1 = FILE_LOCATION + SERIES_NAME + " Vol." + VOLUME_NAME + " #"
+                                                           + ISSUE_NUMBER + " (" + COVER_DATE_1 + ")";
+    private static final String COMIC_FILENAME_RULESET_2 = FILE_LOCATION + SERIES_NAME + " " + ISSUE_NUMBER + " ("
+                                                           + VOLUME_NAME + ") (Digital) (Blah blah) blah.cbr";
+    private static final String COMIC_FILENAME_RULESET_3 = FILE_LOCATION + SERIES_NAME + " (" + VOLUME_NAME
+                                                           + ") (Lots of other crap)";
+
+    private FilenameScraperAdaptor adaptor = new FilenameScraperAdaptor();
+
+    @Mock
+    private Comic comic;
+    @Captor
+    private ArgumentCaptor<Date> cover_date;
+
+    @Test
+    public void testExecuteRuleset1() throws ParseException
+    {
+        Mockito.when(comic.getFilename()).thenReturn(COMIC_FILENAME_RULESET_1);
+        Mockito.doNothing().when(comic).setSeries(Mockito.anyString());
+        Mockito.doNothing().when(comic).setVolume(Mockito.anyString());
+        Mockito.doNothing().when(comic).setIssueNumber(Mockito.anyString());
+        Mockito.doNothing().when(comic).setCoverDate(Mockito.any(Date.class));
+
+        adaptor.execute(comic);
+
+        Mockito.verify(comic, Mockito.times(1)).getFilename();
+        Mockito.verify(comic, Mockito.times(1)).setSeries(SERIES_NAME);
+        Mockito.verify(comic, Mockito.times(1)).setVolume(VOLUME_NAME);
+        Mockito.verify(comic, Mockito.times(1)).setIssueNumber(ISSUE_NUMBER);
+        Mockito.verify(comic, Mockito.times(1)).setCoverDate(new SimpleDateFormat("MMMMM, yyyy").parse(COVER_DATE_1));
+    }
+
+    @Test
+    public void testExecuteRuleset2()
+    {
+        Mockito.when(comic.getFilename()).thenReturn(COMIC_FILENAME_RULESET_2);
+        Mockito.doNothing().when(comic).setSeries(Mockito.anyString());
+        Mockito.doNothing().when(comic).setVolume(Mockito.anyString());
+        Mockito.doNothing().when(comic).setIssueNumber(Mockito.anyString());
+
+        adaptor.execute(comic);
+
+        Mockito.verify(comic, Mockito.times(1)).getFilename();
+        Mockito.verify(comic, Mockito.times(1)).setSeries(SERIES_NAME);
+        Mockito.verify(comic, Mockito.times(1)).setVolume(VOLUME_NAME);
+        Mockito.verify(comic, Mockito.times(1)).setIssueNumber(ISSUE_NUMBER);
+    }
+
+    @Test
+    public void testExecuteRuleset3()
+    {
+        Mockito.when(comic.getFilename()).thenReturn(COMIC_FILENAME_RULESET_3);
+        Mockito.doNothing().when(comic).setSeries(Mockito.anyString());
+        Mockito.doNothing().when(comic).setVolume(Mockito.anyString());
+
+        adaptor.execute(comic);
+
+        Mockito.verify(comic, Mockito.times(1)).getFilename();
+        Mockito.verify(comic, Mockito.times(1)).setSeries(SERIES_NAME);
+        Mockito.verify(comic, Mockito.times(1)).setVolume(VOLUME_NAME);
+    }
+}
