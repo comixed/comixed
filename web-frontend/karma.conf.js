ce88e1ce9aadb087007c31bef25e05da8e19246b
Darryl L. Pierce <mcpierce@gmail.com>
2018-09-08 07:50:18 -0400

    [FIX] Enabled using FirefoxHeadless for running Angular tests.

--- web-frontend/karma.conf.js
+++ web-frontend/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['FirefoxHeadless'],
     singleRun: false
   });
 };
