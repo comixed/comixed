8b728f43b0a6a670e026faab2464e7bee84241a9
Darryl L. Pierce <mcpierce@gmail.com>
2018-09-22 16:45:52 -0400

    [Issue #65] Added the ComicVineQueryForIssuesAdaptor.

---
+++ java-backend/src/main/java/org/comixed/web/comicvine/ComicVineQueryForIssuesAdaptor.java
@@ -0,0 +1,38 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2018, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+package org.comixed.web.comicvine;
+
+import java.util.List;
+
+import org.comixed.web.model.ComicIssue;
+import org.comixed.web.model.ComicVolume;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComicVineQueryForIssuesAdaptor
+{
+    public List<ComicIssue> execute(List<ComicVolume> anyList,
+                                    String apiKey,
+                                    String volume,
+                                    String issueNumber) throws ComicVineAdaptorException
+    {
+        return null;
+    }
+}
