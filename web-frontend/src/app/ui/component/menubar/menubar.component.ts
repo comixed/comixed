c9b6f89fe7521e40dabf7bc508c9bad61a523021
Darryl L. Pierce <mcpierce@gmail.com>
2018-11-05 18:47:19 -0500

    [Issue #281] Added the Menubar component and tests.

---
+++ web-frontend/src/app/ui/component/menubar/menubar.component.ts
@@ -0,0 +1,60 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2018, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.css']
+})
+export class MenubarComponent implements OnInit {
+  CORE_MENU_ITEMS: MenuItem[] = [
+    {
+      label: 'Home',
+      icon: 'fas fa-home',
+      routerLink: ['/home'],
+    },
+    {
+      label: 'Comics',
+      icon: 'fas fa-book',
+      items: [
+        {
+          label: 'Library',
+          icon: 'fas fa-book-reader',
+          routerLink: ['/comics'],
+        },
+        {
+          label: 'Import',
+          icon: 'fas fa-file-upload',
+          routerLink: ['/import'],
+        },
+      ]
+    }
+  ];
+
+  protected items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.items = this.CORE_MENU_ITEMS;
+  }
+}
