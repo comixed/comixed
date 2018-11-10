d1755a3d9b75294957341d152c557fca9a5049c1
Darryl L. Pierce <mcpierce@gmail.com>
2018-11-10 18:04:43 -0500

    [Issue #292] Reworked the library page.
     * Replaced the cover view with a PrimeNG DataView.
     * Deleted the LibrarySidebar component.

--- web-frontend/src/app/ui/pages/library/library-page/library-page.component.ts
+++ web-frontend/src/app/ui/pages/library/library-page/library-page.component.ts
@@ -23,6 +23,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Comic } from '../../../../models/comic.model';
 import { UserService } from '../../../../services/user.service';
 import { ComicService } from '../../../../services/comic.service';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-library-page',
@@ -30,20 +31,26 @@ import { ComicService } from '../../../../services/comic.service';
   styleUrls: ['./library-page.component.css']
 })
 export class LibraryPageComponent implements OnInit {
-  readonly PAGESIZE_PARAMETER = 'pagesize';
+  readonly ROWS_PARAMETER = 'rows';
   readonly SORT_PARAMETER = 'sort';
   readonly COVER_PARAMETER = 'coversize';
   readonly GROUP_BY_PARAMETER = 'groupby';
   readonly TAB_PARAMETER = 'tab';
-  readonly SEARCH_PARAMETER = 'search';
 
   protected comics: Array<Comic> = [];
+  protected selected_comic: Comic;
+  protected show_dialog = false;
+
+  protected rows_options: Array<SelectItem>;
+  protected rows: number;
   protected current_tab: number;
-  protected page_size: number;
   protected cover_size: number;
-  protected sort_order: number;
-  protected group_by: number;
-  protected title_search: string;
+
+  protected sort_options: Array<SelectItem>;
+  protected sort_by: string;
+
+  protected group_options: Array<SelectItem>;
+  protected group_by: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -51,53 +58,83 @@ export class LibraryPageComponent implements OnInit {
     private user_service: UserService,
     private comic_service: ComicService,
   ) {
+    this.sort_options = [
+      { label: 'Series', value: 'series' },
+      { label: 'Date Added', value: 'added_date' },
+      { label: 'Cover Date', value: 'cover_date' },
+      { label: 'Last Read', value: 'last_read_date' },
+    ];
+    this.group_options = [
+      { label: 'None', value: 'none' },
+      { label: 'Series', value: 'series' },
+      { label: 'Publisher', value: 'publisher' },
+      { label: 'Year', value: 'year' },
+    ];
+    this.rows_options = [
+      { label: '10 comics', value: 10 },
+      { label: '25 comics', value: 25 },
+      { label: '50 comics', value: 50 },
+      { label: '100 comics', value: 100 },];
     activatedRoute.queryParams.subscribe(params => {
       this.current_tab = this.load_parameter(params[this.TAB_PARAMETER], 0);
-      this.page_size = this.load_parameter(params[this.PAGESIZE_PARAMETER], 10);
-      this.sort_order = this.load_parameter(params[this.SORT_PARAMETER], 0);
-      this.cover_size = this.load_parameter(params[this.COVER_PARAMETER], 200);
-      this.group_by = this.load_parameter(params[this.GROUP_BY_PARAMETER], 0);
-      this.title_search = this.load_parameter(params[this.SEARCH_PARAMETER], '');
+      this.rows = this.load_parameter(params[this.ROWS_PARAMETER], 10);
+      this.sort_by = params[this.SORT_PARAMETER] || 'series';
+      this.cover_size = this.load_parameter(params[this.COVER_PARAMETER],
+        parseInt(this.user_service.get_user_preference('cover_size', '200'), 10));
+      this.group_by = params[this.GROUP_BY_PARAMETER] || 'none';
     });
   }
 
   ngOnInit() {
-    this.comics = this.comic_service.all_comics_update.subscribe(
+    this.comic_service.all_comics_update.subscribe(
       (comics: Array<Comic>) => {
-        this.comics = this.sort_comics(comics);
-      }
-    );
+        this.comics = comics;
+      });
   }
 
-  set_current_tab(event: any): void {
-    this.current_tab = event.index;
+  set_selected_comic(event: Event, comic: Comic): void {
+    this.selected_comic = comic;
+    this.show_dialog = true;
+    event.preventDefault();
+  }
+
+  hide_dialog(): void {
+    this.show_dialog = false;
+  }
+
+  get_comic_title(comic: Comic): string {
+    return `${comic.series ? comic.series : 'Unknown Series'} ` +
+      `(${comic.volume ? 'v' + comic.volume : '???'}) ` +
+      `${comic.issue_number ? '#' + comic.issue_number : ''}`;
+  }
+
+  get_cover_url(comic: Comic): string {
+    return this.comic_service.get_cover_url_for_comic(comic);
+  }
+
+  set_current_tab(current_tab: number): void {
+    this.current_tab = current_tab;
     this.update_params(this.TAB_PARAMETER, `${this.current_tab}`);
   }
 
-  set_comics_per_page(page_size: number): void {
-    this.page_size = page_size;
-    this.update_params(this.PAGESIZE_PARAMETER, `${this.page_size}`);
+  set_sort_order(sort_order: string): void {
+    this.sort_by = sort_order;
+    this.update_params(this.SORT_PARAMETER, `${this.sort_by}`);
   }
 
-  set_sort_order(sort_order: number): void {
-    this.sort_order = sort_order;
-    this.comics = this.sort_comics(this.comics);
-    this.update_params(this.SORT_PARAMETER, `${this.sort_order}`);
+  set_group_by(group_by: string): void {
+    this.group_by = group_by;
+    this.update_params(this.GROUP_BY_PARAMETER, this.group_by);
   }
 
-  set_search_text(search_text: string): void {
-    this.title_search = search_text;
-    this.update_params(this.SEARCH_PARAMETER, this.title_search);
+  set_rows(rows: string): void {
+    this.rows = parseInt(rows, 10);
+    this.update_params(this.ROWS_PARAMETER, `${this.rows}`);
   }
 
   set_cover_size(cover_size: number): void {
-    this.cover_size = cover_size;
     this.update_params(this.COVER_PARAMETER, `${this.cover_size}`);
-  }
-
-  set_group_by(group_by: number): void {
-    this.group_by = group_by;
-    this.update_params(this.GROUP_BY_PARAMETER, `${this.group_by}`);
+    this.user_service.set_user_preference('cover_size', `${this.cover_size}`);
   }
 
   private update_params(name: string, value: string): void {
@@ -115,73 +152,5 @@ export class LibraryPageComponent implements OnInit {
       return parseInt(value, 10);
     }
     return defvalue;
-  }
-  private sort_comics(comics: Comic[]): Comic[] {
-    comics.sort((comic1: Comic, comic2: Comic) => {
-      switch (this.sort_order) {
-        case 0: return this.sort_by_series_name(comic1, comic2);
-        case 1: return this.sort_by_date_added(comic1, comic2);
-        case 2: return this.sort_by_cover_date(comic1, comic2);
-        case 3: return this.sort_by_last_read_date(comic1, comic2);
-        default: console.log('Invalid sort value: ' + this.sort_order);
-      }
-      return 0;
-    });
-    return comics;
-  }
-
-  private sort_by_series_name(comic1: Comic, comic2: Comic): number {
-    if (comic1.series !== comic2.series) {
-      if (comic1.series < comic2.series) {
-        return -1;
-      } else {
-        return 1;
-      }
-    } else if (comic1.volume !== comic2.volume) {
-      if (comic1.volume < comic2.volume) {
-        return -1;
-      } else {
-        return 1;
-      }
-    } else if (comic1.issue_number !== comic2.issue_number) {
-      if (comic1.issue_number < comic2.issue_number) {
-        return -1;
-      } else {
-        return 1;
-      }
-    }
-
-    // if we're here then the fields are all equal
-    return 0;
-  }
-
-  private sort_by_date_added(comic1: Comic, comic2: Comic): number {
-    if (comic1.added_date < comic2.added_date) {
-      return -1;
-    }
-    if (comic1.added_date > comic2.added_date) {
-      return 1;
-    }
-    return 0;
-  }
-
-  private sort_by_cover_date(comic1: Comic, comic2: Comic): number {
-    if (comic1.cover_date < comic2.cover_date) {
-      return -1;
-    }
-    if (comic1.cover_date > comic2.cover_date) {
-      return 1;
-    }
-    return 0;
-  }
-
-  private sort_by_last_read_date(comic1: Comic, comic2: Comic): number {
-    if (comic1.last_read_date < comic2.last_read_date) {
-      return -1;
-    }
-    if (comic1.last_read_date > comic2.last_read_date) {
-      return 1;
-    }
-    return 0;
   }
 }
