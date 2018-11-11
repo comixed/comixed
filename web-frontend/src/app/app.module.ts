cf83d96502aadb0775eb751d58e7ce5df60bde79
Darryl L. Pierce <mcpierce@gmail.com>
2018-11-11 00:12:21 -0500

    [Issue #280] Relocated the ComicStory component files.

--- web-frontend/src/app/app.module.ts
+++ web-frontend/src/app/app.module.ts
@@ -61,7 +61,7 @@ import { GroupByPipe } from './comic/group-by.pipe';
 import { ComicDetailsEditorComponent } from './ui/components/comics/comic-details-editor/comic-details-editor.component';
 import { IssueDetailsComponent } from './comic/issue/details/issue-details/issue-details.component';
 import { ComicOverviewComponent } from './ui/components/comics/comic-overview/comic-overview.component';
-import { ComicStoryComponent } from './comic/details/story/comic-story/comic-story.component';
+import { ComicStoryComponent } from './ui/components/comics/comic-story/comic-story.component';
 import { ComicCreditsComponent } from './ui/components/comic-credits/comic-credits.component';
 import { ComicPagesComponent } from './comic/details/pages/comic-pages/comic-pages.component';
 import { ComicCoverComponent } from './ui/components/comic-cover/comic-cover.component';
