886d327295ec3207def81b151ad629bcde851915
Darryl L. Pierce <mcpierce@gmail.com>
2019-02-25 21:30:27 -0500

    [Issue #196] Added unit tests for ComicFileCoverUrlPipe.

--- web-frontend/src/app/pipes/comic-file-cover-url.pipe.spec.ts
+++ web-frontend/src/app/pipes/comic-file-cover-url.pipe.spec.ts
@@ -17,11 +17,18 @@
  * org.comixed;
  */
 
-import { ComicFileCoverUrlPipe } from './comic-file-cover-url.pipe';
+import { ComicFileCoverUrlPipe } from "./comic-file-cover-url.pipe";
+import { EXISTING_COMIC_FILE } from "../models/import/comic-file.fixtures";
+import { COMIC_SERVICE_API_URL } from "../services/comic.service";
 
-describe('ComicFileCoverUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ComicFileCoverUrlPipe();
-    expect(pipe).toBeTruthy();
+describe("ComicFileCoverUrlPipe", () => {
+  const pipe = new ComicFileCoverUrlPipe();
+
+  it("returns the url for given page", () => {
+    expect(pipe.transform(EXISTING_COMIC_FILE)).toEqual(
+      `${COMIC_SERVICE_API_URL}/files/import/cover?filename=${encodeURIComponent(
+        EXISTING_COMIC_FILE.filename
+      )}`
+    );
   });
 });
