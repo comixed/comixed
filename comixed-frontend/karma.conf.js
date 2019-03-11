6875ce97cedcc7f7fecb7d693311d65c8a1c58da
Darryl L. Pierce <mcpierce@gmail.com>
2019-03-10 20:30:55 -0400

    [Issue #80] Installed karma-verbose-reporter.

--- comixed-frontend/karma.conf.js
+++ comixed-frontend/karma.conf.js
@@ -1,33 +1,34 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
-	config.set({
-		basePath : '',
-		frameworks : [ 'jasmine', '@angular-devkit/build-angular' ],
-		plugins : [
-			require('karma-jasmine'),
-			require('karma-firefox-launcher'),
-			require('karma-jasmine-html-reporter'),
-			require('karma-coverage-istanbul-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
-		],
-		client : {
-			clearContext : false // leave Jasmine Spec Runner output visible in browser
-		},
-		coverageIstanbulReporter : {
-			reports : [ 'html', 'lcovonly' ],
-			fixWebpackSourcePaths : true
-		},
-		angularCli : {
-			environment : 'dev'
-		},
-		reporters : [ 'progress', 'kjhtml' ],
-		port : 9876,
-		colors : true,
-		logLevel : config.LOG_INFO,
-		autoWatch : true,
-		browsers : [ 'FirefoxHeadless' ],
-		singleRun : true,
-	});
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-firefox-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-verbose-reporter')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly'],
+            fixWebpackSourcePaths: true
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        reporters: ['progress', 'kjhtml', 'verbose'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['FirefoxHeadless'],
+        singleRun: true,
+    });
 };
