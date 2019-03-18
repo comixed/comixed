9004047124b890d93ec25bac1f7d13eb7404c4d0
Darryl L. Pierce <mcpierce@gmail.com>
2019-03-18 12:46:26 -0400

    [Issue #406] Karma now uses Mocha to output its progress.

--- comixed-frontend/karma.conf.js
+++ comixed-frontend/karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
-            require('karma-verbose-reporter')
+            require('karma-verbose-reporter'),
+            require('karma-mocha-reporter'),
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -23,7 +24,8 @@ module.exports = function (config) {
         angularCli: {
             environment: 'dev'
         },
-        reporters: ['verbose', 'kjhtml'],
+        // reporters: ['verbose', 'kjhtml', 'progress'],
+        reporters: ['mocha'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
