1746335739dccf9bc9d08ec7f4a0d1713169946f
Darryl L. Pierce <mcpierce@gmail.com>
2019-04-21 20:48:58 -0400

    [Issue #445] Created the ReadingListRepository class and tests.

--- comixed-library/src/main/java/org/comixed/repositories/ReadingListRepository.java
+++ comixed-library/src/main/java/org/comixed/repositories/ReadingListRepository.java
@@ -1,4 +1,37 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2019, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.package
+ * org.comixed;
+ */
+
 package org.comixed.repositories;
 
-public interface ReadingListRepository {
+import org.comixed.library.lists.ReadingList;
+import org.comixed.library.model.ComiXedUser;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReadingListRepository extends CrudRepository<ReadingList, Long> {
+  @Query("SELECT list FROM ReadingList list WHERE list.owner = :owner")
+  List<ReadingList> findAllReadingListsForUser(ComiXedUser owner);
+
+  @Query("SELECT list FROM ReadingList list WHERE list.owner = :owner AND list.name = :listName")
+  ReadingList findReadingListForUser(ComiXedUser owner, String listName);
 }
