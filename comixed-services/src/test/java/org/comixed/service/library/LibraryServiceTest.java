25745a14568e65846232ee2c59a0f62338771265
Darryl L. Pierce <mcpierce@gmail.com>
2020-01-07 07:43:03 -0500

    [Issue #9] The library service removes comics already returned in previous update.

--- comixed-services/src/test/java/org/comixed/service/library/LibraryServiceTest.java
+++ comixed-services/src/test/java/org/comixed/service/library/LibraryServiceTest.java
@@ -2,25 +2,21 @@ package org.comixed.service.library;
 
 import static junit.framework.TestCase.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.comixed.model.library.Comic;
 import org.comixed.repositories.library.ComicRepository;
 import org.comixed.repositories.tasks.ProcessComicEntryRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LibraryServiceTest {
-  private static final int TEST_PAGE = 20;
-  private static final int TEST_COUNT = 100;
-  private static final String TEST_SORTABLE_FIELD = "coverDate";
-  private static final boolean TEST_ASCENDING = true;
-  private static final long TEST_COMIC_COUNT = 2019;
   private static final String TEST_EMAIL = "reader@comixed.org";
   private static final Date TEST_LAST_UPDATED_TIMESTAMP = new Date();
   private static final int TEST_MAXIMUM_COMICS = 100;
@@ -31,30 +27,55 @@ public class LibraryServiceTest {
   @Mock private ComicRepository comicRepository;
   @Mock private ProcessComicEntryRepository processComicEntryRepository;
   @Captor private ArgumentCaptor<Pageable> pageableArgumentCaptor;
-  @Mock private List<Comic> comicList;
-  @Mock private Date lastUpdatedDate;
-  @Mock private Comic comic;
-  @Mock private Page comicPage;
+
+  private Comic comic1 = new Comic();
+  private Comic comic2 = new Comic();
+  private Comic comic3 = new Comic();
+  private Comic comic4 = new Comic();
+
+  @Before
+  public void setUp() {
+    // updated now
+    comic1.setDateLastUpdated(new Date());
+    comic1.setID(1L);
+    // updated yesterday
+    comic2.setDateLastUpdated(new Date(System.currentTimeMillis() - 24L * 60L * 60L * 1000L));
+    comic2.setID(2L);
+    // updated same as previous comic
+    comic3.setDateLastUpdated(comic2.getDateLastUpdated());
+    comic3.setID(3L);
+    //
+    comic4.setDateLastUpdated(comic3.getDateLastUpdated());
+    comic4.setID(4L);
+  }
 
   @Test
   public void testGetComicsUpdatedSince() {
+    List<Comic> comics = new ArrayList<>();
+    comics.add(comic2);
+    comics.add(comic3);
+    comics.add(comic4);
+
     Mockito.when(
-            comicRepository.getComicsUpdatedSinceDate(
-                Mockito.any(Date.class), Mockito.anyLong(), pageableArgumentCaptor.capture()))
-        .thenReturn(comicList);
+            comicRepository.getLibraryUpdates(
+                Mockito.any(Date.class), pageableArgumentCaptor.capture()))
+        .thenReturn(comics);
 
     List<Comic> result =
         libraryService.getComicsUpdatedSince(
-            TEST_EMAIL, TEST_LAST_UPDATED_TIMESTAMP, TEST_MAXIMUM_COMICS, TEST_LAST_COMIC_ID);
+            TEST_EMAIL, comic3.getDateLastUpdated(), TEST_MAXIMUM_COMICS, 2L);
 
     assertNotNull(result);
-    assertSame(comicList, result);
+    assertFalse(result.isEmpty());
+    assertEquals(2, result.size());
+    assertTrue(result.contains(comic3));
+    assertTrue(result.contains(comic4));
+
     assertEquals(0, pageableArgumentCaptor.getValue().getPageNumber());
     assertEquals(TEST_MAXIMUM_COMICS, pageableArgumentCaptor.getValue().getPageSize());
 
     Mockito.verify(comicRepository, Mockito.times(1))
-        .getComicsUpdatedSinceDate(
-            TEST_LAST_UPDATED_TIMESTAMP, TEST_LAST_COMIC_ID, pageableArgumentCaptor.getValue());
+        .getLibraryUpdates(comic3.getDateLastUpdated(), pageableArgumentCaptor.getValue());
   }
 
   @Test
