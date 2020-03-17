5c248c0161436f2f24e70f5303afed3bb9a99dd2
Darryl L. Pierce <mcpierce@gmail.com>
2020-03-17 19:30:17 -0400

    Revert "[Issue #173] Fix loading the first image from a 7zip encoded archive. (#174)"
    This reverts commit 2b049058f839b83b787a9b5625c10f863168264f.

--- comixed-library/src/main/java/org/comixed/adaptors/archive/SevenZipArchiveAdaptor.java
+++ comixed-library/src/main/java/org/comixed/adaptors/archive/SevenZipArchiveAdaptor.java
@@ -121,7 +121,6 @@ public class SevenZipArchiveAdaptor extends AbstractArchiveAdaptor<SevenZFile> {
   protected byte[] loadSingleFileInternal(SevenZFile archiveReference, String entryName)
       throws ArchiveAdaptorException {
     byte[] result = null;
-
     try {
       boolean done = false;
       SevenZArchiveEntry entry = archiveReference.getNextEntry();
