9ad3ca046e1605b47016da275ea7e90134f5c963
Darryl L. Pierce <mcpierce@gmail.com>
2020-05-08 17:00:37 -0400

    [Issue #208] Only retrieve the publisher's small logo.

--- comixed-rest-api/src/main/java/org/comixed/web/comicvine/ComicVinePublisherDetailsResponseProcessor.java
+++ comixed-rest-api/src/main/java/org/comixed/web/comicvine/ComicVinePublisherDetailsResponseProcessor.java
@@ -107,7 +107,7 @@ public class ComicVinePublisherDetailsResponseProcessor {
       String comicVineUrl = jsonNode.get("results").get("site_detail_url").asText();
       String description = jsonNode.get("results").get("deck").asText();
       String thumbnailUrl = jsonNode.get("results").get("image").get("thumb_url").asText();
-      String logoUrl = jsonNode.get("results").get("image").get("original_url").asText();
+      String logoUrl = jsonNode.get("results").get("image").get("small_url").asText();
 
       if (IMPRINT_MAPPINGS.containsKey(publisher)) {
         this.log.debug("Publisher is an imprint");
