3aef45a68eb663bbcad2d916bc7c1fd0da54ade8
Darryl L. Pierce <mcpierce@gmail.com>
2020-06-07 09:46:51 -0400

    Added the state wiring to remove comics from a reading list [#275]

--- comixed-frontend/src/app/library/models/net/remove-comics-from-reading-list-request.ts
+++ comixed-frontend/src/app/library/models/net/remove-comics-from-reading-list-request.ts
@@ -16,6 +16,6 @@
  * along with this program. If not, see <http:/www.gnu.org/licenses>
  */
 
-export interface AddComicsToReadingListResponse {
-  addCount: number;
+export interface RemoveComicsFromReadingListRequest {
+  ids: number[];
 }
