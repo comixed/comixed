888e79b94d32209e63ee6c80ef8605908f5ec0c6
Darryl L. Pierce <mcpierce@gmail.com>
2020-08-29 09:14:27 -0400

    Added removing deleted comics during library consolidation [#393]

--- comixed-tasks/src/test/java/org/comixedproject/task/model/MoveComicsWorkerTaskTest.java
+++ comixed-tasks/src/test/java/org/comixedproject/task/model/MoveComicsWorkerTaskTest.java
@@ -19,12 +19,14 @@
 package org.comixedproject.task.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.comixedproject.model.comic.Comic;
 import org.comixedproject.model.tasks.Task;
 import org.comixedproject.service.comic.ComicService;
 import org.comixedproject.service.task.TaskService;
 import org.comixedproject.task.encoders.MoveComicWorkerTaskEncoder;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -38,6 +40,7 @@ public class MoveComicsWorkerTaskTest {
   private static final String TEST_DIRECTORY = "/users/comixedreader/Documents/comics";
   private static final String TEST_RENAMING_RULE =
       "$PUBLISHER/$SERIES/$VOLUME/$SERIES v$VOLUME #$ISSUE ($COVERDATE)";
+  private static final int TEST_MOVED_COMICS = 25;
 
   @InjectMocks private MoveComicsWorkerTask moveComicsWorkerTask;
   @Mock private ComicService comicService;
@@ -47,8 +50,16 @@ public class MoveComicsWorkerTaskTest {
   @Mock private Task task;
   @Mock private TaskService taskService;
   @Mock private Comic comic;
+  @Mock private Comic deletedComic;
 
   private List<Comic> comicList = new ArrayList<>();
+
+  @Before
+  public void setUp() {
+    Mockito.when(deletedComic.getDateDeleted()).thenReturn(new Date());
+    for (int index = 0; index < TEST_MOVED_COMICS; index++) comicList.add(comic);
+    comicList.add(deletedComic);
+  }
 
   @Test
   public void testStartTask() throws WorkerTaskException {
@@ -58,19 +69,19 @@ public class MoveComicsWorkerTaskTest {
     Mockito.when(moveComicTaskEncoder.encode()).thenReturn(task);
     Mockito.when(taskService.save(Mockito.any(Task.class))).thenReturn(task);
 
-    for (int index = 0; index < 25; index++) comicList.add(comic);
     moveComicsWorkerTask.setDirectory(TEST_DIRECTORY);
     moveComicsWorkerTask.setRenamingRule(TEST_RENAMING_RULE);
 
     moveComicsWorkerTask.startTask();
 
-    Mockito.verify(moveComicWorkerTaskEncoderObjectFactory, Mockito.times(comicList.size()))
+    Mockito.verify(moveComicWorkerTaskEncoderObjectFactory, Mockito.times(TEST_MOVED_COMICS))
         .getObject();
-    Mockito.verify(moveComicTaskEncoder, Mockito.times(comicList.size())).setComic(comic);
-    Mockito.verify(moveComicTaskEncoder, Mockito.times(comicList.size()))
+    Mockito.verify(moveComicTaskEncoder, Mockito.times(TEST_MOVED_COMICS)).setComic(comic);
+    Mockito.verify(moveComicTaskEncoder, Mockito.times(TEST_MOVED_COMICS))
         .setTargetDirectory(TEST_DIRECTORY);
-    Mockito.verify(moveComicTaskEncoder, Mockito.times(comicList.size()))
+    Mockito.verify(moveComicTaskEncoder, Mockito.times(TEST_MOVED_COMICS))
         .setRenamingRule(TEST_RENAMING_RULE);
-    Mockito.verify(taskService, Mockito.times(comicList.size())).save(task);
+    Mockito.verify(taskService, Mockito.times(TEST_MOVED_COMICS)).save(task);
+    Mockito.verify(comicService, Mockito.times(1)).delete(deletedComic);
   }
 }
