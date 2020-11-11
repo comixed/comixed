1f8ea769d4369ec913a475bd7937231b1058fd08
Darryl L. Pierce <mcpierce@gmail.com>
2020-11-11 15:07:35 -0500

    Added dependency on Angular Material [#328]

--- comixed-web/src/app/app.module.ts
+++ comixed-web/src/app/app.module.ts
@@ -20,10 +20,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
