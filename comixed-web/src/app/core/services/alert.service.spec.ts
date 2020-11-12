da7be6152243fa71da4e3c4005a6134d42828668
Darryl L. Pierce <mcpierce@gmail.com>
2020-11-12 07:49:39 -0500

    Added an alert service [#328]

---
+++ comixed-web/src/app/core/services/alert.service.spec.ts
@@ -0,0 +1,90 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2020, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>
+ */
+
+import { TestBed } from '@angular/core/testing';
+import {
+  ALERT_HORZ_POSITION,
+  ALERT_VERT_POSITION,
+  AlertService,
+  ERROR_MESSAGE_DURATION,
+  INFO_MESSAGE_DURATION
+} from './alert.service';
+import { LoggerModule } from '@angular-ru/logger';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { TranslateModule } from '@ngx-translate/core';
+
+describe('AlertService', () => {
+  const TEST_MESSAGE = 'This is the alert message';
+
+  let service: AlertService;
+  let snackbar: MatSnackBar;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        LoggerModule.forRoot(),
+        TranslateModule.forRoot(),
+        MatSnackBarModule
+      ]
+    });
+
+    service = TestBed.inject(AlertService);
+    snackbar = TestBed.inject(MatSnackBar);
+    spyOn(snackbar, 'open');
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('showing an information alert', () => {
+    beforeEach(() => {
+      service.info(TEST_MESSAGE);
+    });
+
+    it('opens the message popup', () => {
+      expect(snackbar.open).toHaveBeenCalledWith(
+        TEST_MESSAGE,
+        jasmine.any(String),
+        jasmine.objectContaining({
+          duration: INFO_MESSAGE_DURATION,
+          horizontalPosition: ALERT_HORZ_POSITION,
+          verticalPosition: ALERT_VERT_POSITION
+        })
+      );
+    });
+  });
+
+  describe('showing an error alert', () => {
+    beforeEach(() => {
+      service.error(TEST_MESSAGE);
+    });
+
+    it('opens the message popup', () => {
+      expect(snackbar.open).toHaveBeenCalledWith(
+        TEST_MESSAGE,
+        jasmine.any(String),
+        jasmine.objectContaining({
+          duration: ERROR_MESSAGE_DURATION,
+          horizontalPosition: ALERT_HORZ_POSITION,
+          verticalPosition: ALERT_VERT_POSITION
+        })
+      );
+    });
+  });
+});
---
+++ comixed-web/src/app/core/services/alert.service.ts
@@ -0,0 +1,69 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2020, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>
+ */
+
+import { Injectable } from '@angular/core';
+import { LoggerService } from '@angular-ru/logger';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
+
+export const INFO_MESSAGE_DURATION = 500;
+export const ERROR_MESSAGE_DURATION = 0;
+export const ALERT_HORZ_POSITION = 'end';
+export const ALERT_VERT_POSITION = 'top';
+
+/**
+ * AlertService shows message toasters to the user.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  constructor(
+    private logger: LoggerService,
+    private translateService: TranslateService,
+    private snackbar: MatSnackBar
+  ) {}
+
+  /** Shows an information alert. */
+  info(message: string): void {
+    this.logger.trace('Showing info message:', message);
+    this.snackbar.open(
+      message,
+      this.translateService.instant('alert.info-action'),
+      {
+        duration: INFO_MESSAGE_DURATION,
+        horizontalPosition: ALERT_HORZ_POSITION,
+        verticalPosition: ALERT_VERT_POSITION
+      }
+    );
+  }
+
+  /** Shows an error alert. */
+  error(message: string): void {
+    this.logger.trace('Showing error message:', message);
+    this.snackbar.open(
+      message,
+      this.translateService.instant('alert.error-action'),
+      {
+        duration: ERROR_MESSAGE_DURATION,
+        horizontalPosition: ALERT_HORZ_POSITION,
+        verticalPosition: ALERT_VERT_POSITION
+      }
+    );
+  }
+}
