29da59fc70b2597428229c0c7f6e62a3e5a2dce2
Darryl L. Pierce <mcpierce@gmail.com>
2020-12-30 07:36:41 -0500

    Added a reusable view for displaying lists of comics [#573]

---
+++ comixed-web/src/app/library/components/comic-covers/comic-covers.component.ts
@@ -0,0 +1,33 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2020, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>
+ */
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Comic } from '@app/library';
+
+@Component({
+  selector: 'cx-comic-covers',
+  templateUrl: './comic-covers.component.html',
+  styleUrls: ['./comic-covers.component.scss']
+})
+export class ComicCoversComponent implements OnInit {
+  @Input() comics: Comic[];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
