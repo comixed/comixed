d6bc83cb4e5f90647cf99a7ee28cdb3a8b25e5b6
Darryl L. Pierce <mcpierce@gmail.com>
2021-06-29 07:28:37 -0400

    Changed the development version to 0.10.0-SNAPSHOT [#768]

--- comixed-release/build-installer.bat
+++ comixed-release/build-installer.bat
@@ -1,4 +1,4 @@
 @echo off
 
-jar xvf ..\release\comixed-release-0.9.0-SNAPSHOT-local.zip
+jar xvf ..\release\comixed-release-0.10.0-SNAPSHOT-local.zip
 "c:\Program Files (x86)\Inno Setup 6\ISCC.exe" src\main\windows\installer.iss
