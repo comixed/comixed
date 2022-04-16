8c09a5afe809b73759e4944b61d20ab16cf88e59
Darryl L. Pierce <mcpierce@gmail.com>
2022-04-16 09:45:28 -0400

    Fixed whitelabel error page [#1223]

--- comixed-auth/src/main/java/org/comixedproject/auth/ComiXedMvcWebConfiguration.java
+++ comixed-auth/src/main/java/org/comixedproject/auth/ComiXedMvcWebConfiguration.java
@@ -42,7 +42,7 @@ public class ComiXedMvcWebConfiguration implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(final ResourceHandlerRegistry registry) {
     registry
-        .addResourceHandler("/*")
+        .addResourceHandler("/**")
         .addResourceLocations("classpath:/static/")
         .resourceChain(true)
         .addResolver(
