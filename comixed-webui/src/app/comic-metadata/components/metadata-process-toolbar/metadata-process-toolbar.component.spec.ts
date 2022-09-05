3d9f8a76cecb560bdaace407700d9cf0d30b9f13
Darryl L. Pierce <mcpierce@gmail.com>
2022-09-05 17:06:42 -0400

    Added setting the skip cache flag for metadata batch process [#1401]

--- comixed-webui/src/app/comic-metadata/components/metadata-process-toolbar/metadata-process-toolbar.component.spec.ts
+++ comixed-webui/src/app/comic-metadata/components/metadata-process-toolbar/metadata-process-toolbar.component.spec.ts
@@ -27,9 +27,24 @@ import {
   ConfirmationService
 } from '@tragically-slick/confirmation';
 import { startMetadataUpdateProcess } from '@app/comic-metadata/actions/metadata.actions';
+import {
+  initialState as initialUserState,
+  USER_FEATURE_KEY
+} from '@app/user/reducers/user.reducer';
+import { USER_ADMIN } from '@app/user/user.fixtures';
+import { SKIP_CACHE_PREFERENCE } from '@app/library/library.constants';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { saveUserPreference } from '@app/user/actions/user.actions';
 
 describe('MetadataProcessToolbarComponent', () => {
-  const initialState = {};
+  const USER = USER_ADMIN;
+  const SKIP_CACHE = Math.random() > 0.5;
+  const initialState = {
+    [USER_FEATURE_KEY]: { ...initialUserState, user: { ...USER } }
+  };
   const IDS = [3, 20, 96, 9, 21, 98, 4, 17, 6];
 
   let component: MetadataProcessToolbarComponent;
@@ -41,9 +56,13 @@ describe('MetadataProcessToolbarComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [MetadataProcessToolbarComponent],
       imports: [
+        NoopAnimationsModule,
         LoggerModule.forRoot(),
         TranslateModule.forRoot(),
-        MatDialogModule
+        MatDialogModule,
+        MatTooltipModule,
+        MatToolbarModule,
+        MatIconModule
       ],
       providers: [provideMockStore({ initialState }), ConfirmationService]
     }).compileComponents();
@@ -60,12 +79,60 @@ describe('MetadataProcessToolbarComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('setting the skip cache flag', () => {
+    describe('if defined', () => {
+      beforeEach(() => {
+        component.skipCache = !SKIP_CACHE;
+        store.setState({
+          ...initialState,
+          [USER_FEATURE_KEY]: {
+            ...initialUserState,
+            user: {
+              ...USER,
+              preferences: [
+                {
+                  name: SKIP_CACHE_PREFERENCE,
+                  value: `${SKIP_CACHE}`
+                }
+              ]
+            }
+          }
+        });
+      });
+
+      it('sets the flag from user preferences', () => {
+        expect(component.skipCache).toEqual(SKIP_CACHE);
+      });
+    });
+
+    describe('if not defined', () => {
+      beforeEach(() => {
+        component.skipCache = SKIP_CACHE;
+        store.setState({
+          ...initialState,
+          [USER_FEATURE_KEY]: {
+            ...initialUserState,
+            user: {
+              ...USER,
+              preferences: null
+            }
+          }
+        });
+      });
+
+      it('turns the flag off', () => {
+        expect(component.skipCache).toEqual(false);
+      });
+    });
+  });
+
   describe('starting the metadata update process', () => {
     beforeEach(() => {
       spyOn(confirmationService, 'confirm').and.callFake(
         (confirmation: Confirmation) => confirmation.confirm()
       );
       component.selectedIds = IDS;
+      component.skipCache = SKIP_CACHE;
       component.onStartBatchProcess();
     });
 
@@ -75,7 +142,23 @@ describe('MetadataProcessToolbarComponent', () => {
 
     it('fires an action', () => {
       expect(store.dispatch).toHaveBeenCalledWith(
-        startMetadataUpdateProcess({ ids: IDS, skipCache: false })
+        startMetadataUpdateProcess({ ids: IDS, skipCache: SKIP_CACHE })
+      );
+    });
+  });
+
+  describe('toggling the skip cache preference', () => {
+    beforeEach(() => {
+      component.skipCache = SKIP_CACHE;
+      component.onToggleSkipCache();
+    });
+
+    it('fires an action', () => {
+      expect(store.dispatch).toHaveBeenCalledWith(
+        saveUserPreference({
+          name: SKIP_CACHE_PREFERENCE,
+          value: `${!SKIP_CACHE}`
+        })
       );
     });
   });
--- comixed-webui/src/app/comic-metadata/components/metadata-process-toolbar/metadata-process-toolbar.component.ts
+++ comixed-webui/src/app/comic-metadata/components/metadata-process-toolbar/metadata-process-toolbar.component.ts
@@ -16,27 +16,53 @@
  * along with this program. If not, see <http://www.gnu.org/licenses>
  */
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { LoggerService } from '@angular-ru/cdk/logger';
 import { Store } from '@ngrx/store';
 import { ConfirmationService } from '@tragically-slick/confirmation';
 import { TranslateService } from '@ngx-translate/core';
 import { startMetadataUpdateProcess } from '@app/comic-metadata/actions/metadata.actions';
+import { saveUserPreference } from '@app/user/actions/user.actions';
+import { SKIP_CACHE_PREFERENCE } from '@app/library/library.constants';
+import { Subscription } from 'rxjs';
+import { selectUser } from '@app/user/selectors/user.selectors';
+import { getUserPreference } from '@app/user';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-metadata-process-toolbar',
   templateUrl: './metadata-process-toolbar.component.html',
   styleUrls: ['./metadata-process-toolbar.component.scss']
 })
-export class MetadataProcessToolbarComponent {
+export class MetadataProcessToolbarComponent implements OnDestroy {
   @Input() selectedIds: number[] = [];
+  userSubscription: Subscription;
+  skipCache = false;
 
   constructor(
     private logger: LoggerService,
     private store: Store<any>,
     private confirmationService: ConfirmationService,
     private translateService: TranslateService
-  ) {}
+  ) {
+    this.logger.trace('Subscribing to user updates');
+    this.userSubscription = this.store
+      .select(selectUser)
+      .pipe(filter(user => !!user))
+      .subscribe(user => {
+        this.skipCache =
+          getUserPreference(
+            user.preferences || [],
+            SKIP_CACHE_PREFERENCE,
+            `${false}`
+          ) === `${true}`;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.logger.trace('Unsubscribing from user updates');
+    this.userSubscription.unsubscribe();
+  }
 
   onStartBatchProcess(): void {
     this.confirmationService.confirm({
@@ -52,10 +78,20 @@ export class MetadataProcessToolbarComponent {
         this.store.dispatch(
           startMetadataUpdateProcess({
             ids: this.selectedIds,
-            skipCache: false
+            skipCache: this.skipCache
           })
         );
       }
     });
+  }
+
+  onToggleSkipCache(): void {
+    this.logger.trace('Saving skip cache preference:', !this.skipCache);
+    this.store.dispatch(
+      saveUserPreference({
+        name: SKIP_CACHE_PREFERENCE,
+        value: `${!this.skipCache}`
+      })
+    );
   }
 }
