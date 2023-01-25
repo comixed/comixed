7913f0fe6ebf507d215e0afdc74d53b2908982f3
Darryl L. Pierce <mcpierce@gmail.com>
2023-01-24 19:23:48 -0500

    Changed the release version to v1.3.1-1 [#1478]

--- comixed-release/build-installer.bat
+++ comixed-release/build-installer.bat
@@ -1,4 +1,4 @@
 @echo off
 
-jar xvf ..\release\comixed-release-1.3.0-1-local.zip
+jar xvf ..\release\comixed-release-1.3.1-1-local.zip
 "c:\Program Files (x86)\Inno Setup 6\ISCC.exe" src\main\windows\installer.iss
