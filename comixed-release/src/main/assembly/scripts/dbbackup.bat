1613116afa87d45fc484a246ba696bc46dbf8766
Darryl L. Pierce <mcpierce@gmail.com>
2023-09-13 07:09:16 -0400

    Added scripts to backup and restore a database [#1439]

---
+++ comixed-release/src/main/assembly/scripts/dbbackup.bat
@@ -0,0 +1,83 @@
+@ECHO OFF
+
+REM ComiXed - A digital comic book library management application.
+REM Copyright (C) 2021, The ComiXed Project
+REM
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program. If not, see <http://www.gnu.org/licenses>
+
+SETLOCAL
+CD /d %~dp0
+
+SET DATABASE=~/.comixed/comixed
+SET DBUSER=sa
+
+:process_command_line
+IF "%~1" == "" GOTO end_process_command_line
+SET PARAM=%~1
+SET ARG=%~2
+IF "%PARAM%" == "-h" GOTO show_help
+IF "%PARAM%" == "-d" GOTO set_database
+IF "%PARAM%" == "-u" GOTO set_jdbc_user
+IF "%PARAM%" == "-p" GOTO set_jdbc_pwrd
+SET ARGUMENTS="%ARGUMENTS% %PARAM%"
+SHIFT
+GOTO process_command_line
+
+:set_database
+SET DATABASE=%ARG%
+SHIFT
+SHIFT
+GOTO process_command_line
+
+:set_jdbc_user
+SET DBUSER=%ARG%
+SHIFT
+SHIFT
+GOTO process_command_line
+
+:set_jdbc_pwrd
+SET DBPWRD=%ARG%
+SHIFT
+SHIFT
+GOTO process_command_line
+
+:show_help
+ECHO Usage: dbbackup.bat  -d [DATABASE] -j [URL] [OPTIONS]
+ECHO.
+ECHO OPTIONS:
+ECHO  -d [DATABASE] - The database name (def. ~/.comixed/comixed)
+ECHO  -j [URL]      - Set the database URL
+ECHO  -u [USERNAME] - Set the database username
+ECHO  -p [PASSWORD] - Set the database password
+ECHO.
+ECHO OTHER OPTIONS:
+ECHO  -h            - Show help (this text)
+ECHO.
+GOTO exit_script
+
+:end_process_command_line
+
+SET OPTIONS=
+IF "%DBUSER%" == "" GOTO skip_set_user
+SET OPTIONS=%OPTIONS% -user %DBUSER%
+:skip_set_user
+
+IF "%DBPWRD%" == "" GOTO skip_set_password
+SET OPTIONS=%OPTIONS% -password %DBPWRD%
+:skip_set_password
+
+java -cp %LIBDIR%/h2*jar org.h2.tools.Script -url jdbc:h2:file:%DATABASE% %OPTIONS% -script comixed-backup-1.6-SNAPSHOT.zip -options compression zip
+
+:exit_script
+ENDLOCAL
---
+++ comixed-release/src/main/assembly/scripts/dbrestore.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+#
+# ComiXed - A digital comic book library management application.
+# Copyright (C) 2021, The ComiXed Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses>
+
+realpath() {
+  [[ $1 == /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+ME=$(realpath "$0")
+BINDIR=$(dirname "${ME}")
+LIBDIR=$(readlink -f "${BINDIR}"/../lib)
+
+JAVA=$(which java)
+JAROPTIONS=""
+JVMOPTIONS=""
+DATABASE="~/.comixed/comixed"
+DBUSER="sa"
+DBPWRD=""
+
+function usage() {
+  local message="$1"
+
+  echo "Usage: dbrestore.sh FILENAME -d [DATABASE] -j [URL] [OPTIONS]"
+  if [[ ! -z ${message} ]]; then
+    echo ""
+    echo "${message}"
+  fi
+
+  echo ""
+  echo " FILENAME      - The backup filename"
+  echo ""
+  echo "OPTIONS:"
+  echo " -d [DATABASE] - Database type (def. ~/.comixed/comixed)"
+  echo " -u [USERNAME] - Set the database username"
+  echo " -p [PASSWORD] - Set the database password"
+  echo ""
+  echo "Other options:"
+  echo " -h            - Show help (this text)"
+  echo ""
+  exit 0
+}
+
+while getopts "d:u:p:" option; do
+  case ${option} in
+  d) DATABASE="${OPTARG}" ;;
+  u) DBUSER="${OPTARG}" ;;
+  p) DBPWRD="${OPTARG}" ;;
+  \?) usage ;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+if [[ ! -z "${@}" ]]; then
+  BACKUP_FILE=${@}
+fi
+
+if [[ -z "${BACKUP_FILE}" ]]; then
+  usage
+fi
+
+OPTIONS=""
+if [[ ! -z "${DBUSER}" ]]; then OPTIONS="${OPTIONS} -user ${DBUSER}"; fi
+if [[ ! -z "${DBPWRD}" ]]; then OPTIONS="${OPTIONS} -password ${DBPWRD}"; fi
+
+java -cp $LIBDIR/h2*jar org.h2.tools.RunScript -url jdbc:h2:file:${DATABASE} ${OPTIONS} -script ${BACKUP_FILE} -options compression zip
+
+exit 0
