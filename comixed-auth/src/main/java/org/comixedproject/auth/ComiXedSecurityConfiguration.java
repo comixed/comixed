805f9bab525de93d1cfb0bcfcc730e5737a73525
Darryl L. Pierce <mcpierce@gmail.com>
2023-11-03 16:30:47 -0400

    Changed web security to use the Spring security filter chains [#1296]

---
+++ comixed-auth/src/main/java/org/comixedproject/auth/ComiXedSecurityConfiguration.java
@@ -0,0 +1,116 @@
+/*
+ * ComiXed - A digital comic book library management application.
+ * Copyright (C) 2023, The ComiXed Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>
+ */
+
+package org.comixedproject.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * <code>ComiXedSecurityConfiguration</code> provides the security configuration for all web-based
+ * server access endpoints.
+ *
+ * @author Darryl L. Pierce
+ */
+@Configuration
+public class ComiXedSecurityConfiguration {
+  @Autowired private ComiXedAuthenticationFilter authenticationFilter;
+  @Autowired private ComiXedAuthenticationProvider authenticationProvider;
+  @Autowired private ComiXedUnauthorizedEntryPoint unauthorizedHandler;
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  /**
+   * Provides a security filter chain for the REST APIs.
+   *
+   * @param http the security builder
+   * @return the filter chain
+   * @throws Exception if an error occurs
+   */
+  @Bean
+  public SecurityFilterChain restSecurityFilterChain(final HttpSecurity http) throws Exception {
+    http.cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
+        .authenticationProvider(this.authenticationProvider)
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(this.unauthorizedHandler)
+        .and()
+        .antMatcher("/api/**")
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll();
+    http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  /**
+   * Provides a security filter chain for the OPDS APIs.
+   *
+   * @param http the security builder
+   * @return the filter chain
+   * @throws Exception if an error occurs
+   */
+  @Bean
+  public SecurityFilterChain opdsSecurityFilterChain(final HttpSecurity http) throws Exception {
+    http.antMatcher("/opds/**")
+        .authorizeRequests()
+        .anyRequest()
+        .hasRole("READER")
+        .and()
+        .httpBasic();
+    http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public SecurityFilterChain runtimeSecurityFilterChain(final HttpSecurity http) throws Exception {
+    http.antMatcher("/actuator/**")
+        .cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeRequests()
+        .anyRequest()
+        .hasRole("ADMIN")
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(this.unauthorizedHandler)
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+    http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+}
