81ffea20c912852878a734f351904005ca4a4bcd
Darryl L. Pierce <mcpierce@gmail.com>
2023-12-23 11:45:03 -0500

    Removed the Duplicate Comics page [#1831]

--- comixed-webui/src/app/library/library.routing.ts
+++ comixed-webui/src/app/library/library.routing.ts
@@ -22,7 +22,6 @@ import { AdminGuard, ReaderGuard } from '@app/user';
 import { LibraryPageComponent } from '@app/library/pages/library-page/library-page.component';
 import { DuplicatePageListPageComponent } from '@app/library/pages/duplicate-page-list-page/duplicate-page-list-page.component';
 import { DuplicatePageDetailPageComponent } from '@app/library/pages/duplicate-page-detail-page/duplicate-page-detail-page.component';
-import { DuplicateComicsPageComponent } from '@app/library/pages/duplicate-comics-page/duplicate-comics-page.component';
 
 const routes: Routes = [
   {
@@ -59,11 +58,6 @@ const routes: Routes = [
     component: LibraryPageComponent,
     canActivate: [AdminGuard],
     data: { deleted: true }
-  },
-  {
-    path: 'library/comics/duplicates',
-    component: DuplicateComicsPageComponent,
-    canActivate: [AdminGuard]
   },
   {
     path: 'library/pages/duplicates',
