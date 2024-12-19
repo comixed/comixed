e8577db2b6feaea88545cc16214541c64ec61314
Darryl L. Pierce <mcpierce@gmail.com>
2024-12-19 08:58:34 -0500

    Changed the release version to v2.2.5-1 [#2157]

--- comixed-release/build-installer.bat
+++ comixed-release/build-installer.bat
@@ -1,4 +1,4 @@
 @echo off
 
-jar xvf ..\release\comixed-release-2.2.4-1-local.zip
+jar xvf ..\release\comixed-release-2.2.5-1-local.zip
 "c:\Program Files (x86)\Inno Setup 6\ISCC.exe" src\main\windows\installer.iss
